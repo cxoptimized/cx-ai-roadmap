3b39cf6cd748aff4e29fd2f510c1a8be8ec1175d
cxoptimized <cxoptimized@gmail.com>
2025-06-05 09:22:59 -0400

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to CX Optimized</h1>
-      <p>Use the tool to generate your custom AI Integration Roadmap.</p>
-    </main>
-  );
-}
